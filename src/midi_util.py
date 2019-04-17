2fc8405eca1d6705c0088c3e8af822c50cb5fbf0
topologyYDM <topologyYDM@gmail.com>
2019-04-17 21:41:38 +0800

    update all util

--- src/midi_util.py
+++ src/midi_util.py
@@ -1,36 +1,18 @@
 import midi
 import sys
+from util import _read_general_midi
+from track_util import _get_tempo, _get_time_signature, \
+    _set_tempo, _set_time_signature
+
+# 只是为了demo使用
+from track_util import get_note, get_channel_program, \
+    set_note, set_program
+
 '''
     Tempo - 一个拍的时间，比如120BPM表示1min120beats，所以是500,000ms一个beat
     Resolution - 一个beat含有的tick数，tick是midi中最小的时间单位（相对的，由Tempo和Resolution定出绝对值）。如Resolution为1,000，则一个tick500ms
     Time Signature - 拍号，在midi中不会影响到Tempo
 '''
-def read_general_midi():
-    """
-    获取general_midi
-    """
-    with open("./general_midi") as f:
-        lines = f.readlines()
-        idx, program = [], []
-        for line in lines:
-            if line:
-                line = line.split()
-                idx.append(int(line[0]))
-                program.append('_'.join(line[1:]))
-    return idx, program
-
-def get_program_drum(track):
-    '''
-    从打击乐track中提取乐器表
-    '''
-    program = set()
-    for event in track:
-        if isinstance(event, midi.NoteOnEvent) or isinstance(event, midi.NoteOffEvent):
-            try:
-                program.add(event.data[0])
-            except AttributeError:
-                pass
-    return list(program)
 
 def gen_multitracks(pattern):
     """
@@ -49,11 +31,11 @@ def gen_multitracks(pattern):
                     channel = event.channel
                     _sub_pattern.append(track)
                     #判断是旋律音色还是打击乐
-                    program = event.data if channel != 9 else get_program_drum(track)
+                    program = event.data if channel != 9 else "Percusiion"
                     sub_pattern = {
                         'pattern': _sub_pattern,
                         'channel': channel,
-                        'program':program  
+                        'program': program  
                     }
                     #分离track
                     sub_patterns.append(sub_pattern)
@@ -79,30 +61,107 @@ def save_multitracks(multitracks, root="../track/", name="untitled"):
     """
     将multitracks保存
     """
-    idx, program = read_general_midi()
+    idx, program = _read_general_midi()
     for track in multitracks:
         p = idx.index(track["program"][0])
         program_name = program[p] if track["channel"] != 9 else "Drum"
         filename = root + name + "_" + str(track["channel"])\
                         + "_" + program_name + ".mid"    
         midi.write_midifile(filename, track["pattern"])
-        
-def demo_multitracks(song):
-    midiname = "../midi/" + song + ".mid"
-    test_pattern = read_midifile(midiname)
-    test_multitracks = gen_multitracks(test_pattern)
-    save_multitracks(test_multitracks, name=song)
 
 def get_tempo(pattern):
     '''
     遍历pattern,通过midi.SetTempoEvent得到tempo
     '''
     for track in pattern:
-        for event in track:
-            if isinstance(event, midi.SetTempoEvent): 
-                return event.get_bpm()
+        bpm = _get_tempo(track)
+        if bpm == 0:
+            continue
+        else:
+            return bpm
+    
+    print("! : No tempo INFO!")
+    return 0
+
+def get_time_signature(pattern):
+    """
+    遍历pattern,通过midi.TimeSignatureEvent得到time signature
+    """
+    for track in pattern:
+        data = _get_time_signature(track)
+        if data == 0:
+            continue
+        else:
+            return data
+
+    print("! : No time signature INFO!")
+    return 0
+
+def get_resolution(pattern):
+    """
+    返回resolution
+    """
+    return pattern.resolution
+
+def set_tempo(pattern, bpm):
+    """
+    设置一个Pattern的tempo
+    放在第一个Track中(第一个Track只记录信息)
+    """
+    track = _set_tempo(pattern[0], bpm)
+    pattern[0] = track
+    return pattern
+
+def set_time_signature(pattern, data):
+    """
+    设置一个Pattern的time signature
+    放在第一个Track中(第一个Track只记录信息)
+    """
+    track = _set_time_signature(pattern[0], data)
+    pattern[0] = track
+    return pattern
+
+def set_resolution(pattern, resolution):
+    """
+    设置一个Pattern的resolution
+    """
+    pattern.resolution = resolution
+    return pattern
+    
+def demo_multitracks(song):
+    midiname = "../midi/" + song + ".mid"
+    test_pattern = read_midifile(midiname)
+    test_multitracks = gen_multitracks(test_pattern)
+    save_multitracks(test_multitracks, name=song)
+
+def demo():
+    # 这个demo是把我们分出的单音色的Pattern标准化
+    # 这样的Pattern有两个Track
+    # Track0 仅有一些基本信息
+    # Track1 包含了音乐本身
+    midifile = "../track/Stan_1_Violin.mid"
+    pattern = read_midifile(midifile)
+    
+    resolution = get_resolution(pattern)
+    bpm = get_tempo(pattern)
+    time_signature = get_time_signature(pattern)
+    note = get_note(pattern[1])
+    program = get_channel_program(pattern[1]) # [channel, program]
+
+    # 输出Pattern的初始化
+    pat = midi.Pattern(tracks=[[],[]])
+
+    pat = set_resolution(pat, resolution)
+    pat = set_tempo(pat, bpm)
+    pat = set_time_signature(pat, time_signature)
+
+    pat[1] = set_program(pat[1], channel=program[0], value=40)
+    pat[1] = set_note(pat[1], note)
+
+    save_midfile(pat, "../test_std_Stan.mid")
 
 if __name__ == '__main__':
-    song = 'Stan'
-    demo_multitracks(song)
+    # song = 'Stan'
+    # demo_multitracks(song)
+    demo()
     
--- src/util.py
+++ src/util.py
@@ -1,6 +1,7 @@
+import midi
 import numpy as np
 
-def cal_var(l):
+def _cal_var(l):
     """
     list = [...]
     计算直到第i位list[:i]方差大于1
@@ -9,3 +10,17 @@ def cal_var(l):
         if np.var(l[:i]) / i > 0.1:
             return i - 1
     return 0
+
+def _read_general_midi():
+    """
+    获取general_midi
+    """
+    with open("./general_midi") as f:
+        lines = f.readlines()
+        idx, program = [], []
+        for line in lines:
+            if line:
+                line = line.split()
+                idx.append(int(line[0]))
+                program.append('_'.join(line[1:]))
+    return idx, program
